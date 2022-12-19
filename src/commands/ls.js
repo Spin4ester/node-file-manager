2e5b861260b2cc837c98de7dadd36cc963e3c189
Spin4ester <bakibayev.r@gmail.com>
2022-12-19 23:58:37 +0100

    fix: fix sorting in ls function

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -9,12 +9,19 @@ export default async function handleLs() {
     const curDir = resolve(cwd());
     const files = await readdir(curDir);
     const checkDir = await Promise.all(files.map((el) => isDir(el)));
-    const obj = files.map((el) => {
-      return {
-        Name: el,
-        Type: checkDir[files.indexOf(el)] === true ? 'directory' : 'file',
-      };
-    });
+    const obj = files
+      .map((el) => {
+        return {
+          Name: el,
+          Type: checkDir[files.indexOf(el)] === true ? 'directory' : 'file',
+        };
+      })
+      .sort((a, b) => {
+        if (a.Type > b.Type) return 1;
+        if (a.Type < b.Type) return -1;
+        if (a.Name > b.Name) return 1;
+        if (a.Name < b.Name) return -1;
+      });
     console.table(obj);
     displayCurDir();
   } catch (error) {
